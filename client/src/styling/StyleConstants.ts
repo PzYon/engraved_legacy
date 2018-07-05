1ef48cb6a90ddcf2e10a0708d36c16b9f43dd96a
Markus Doggweiler <PzYon@users.noreply.github.com>
2018-07-05 22:37:11 +0200

    #21: welcome screen (#26)
    plus some files that seemed to have
    gotten lost somewhere - whuuuat?

---
+++ client/src/styling/StyleConstants.ts
@@ -0,0 +1,28 @@
+const accentColor = "#0099cc";
+
+export const StyleConstants = {
+    headerHeightInPx: 50,
+    maxContentWidth: "1200px",
+    defaultPadding: "10px",
+    defaultMargin: "10px",
+    largeFontSize: "1.2rem",
+    formElementPadding: "4px",
+    fontFamily: "'IBM Plex Sans', sans-serif",
+    defaultBoxShadow: `3px 3px 10px ${accentColor}`,
+    colors: {
+        header: {
+            background: "#24292e",
+            text: "white"
+        },
+        pageBackground: accentColor,
+        contentBackground: "white",
+        font: "#333333",
+        accent: accentColor,
+        borders: accentColor,
+        error: "darkred",
+        success: {
+            backgroundTransparent: "rgba(0, 102, 0, 0.8)",
+            text: "white"
+        }
+    }
+};
