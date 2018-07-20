53db86ea2bd3e1fa5ec8a25d91bcce32485a5e51
Markus Doggweiler <markus.doggweiler@gmail.com>
2018-07-20 13:19:24 +0200

    #34, #41: CodeEditor.tsx use codemirror instead of Ace

--- client/src/common/form/fields/CodeEditor.tsx
+++ client/src/common/form/fields/CodeEditor.tsx
@@ -1,24 +1,17 @@
-// note: the following lines are required, or else ace editor won't work and throw an exception that "ace" is not defined
-// the reason for this are the strange imports, they assume that a variable called "ace" is available
-import * as brace from "brace";
-import "brace/ext/searchbox";
-import "brace/mode/csharp";
-import "brace/mode/javascript";
-import "brace/mode/json";
-import "brace/mode/markdown";
-import "brace/mode/typescript";
-import "brace/theme/github";
+import "codemirror/lib/codemirror.css";
 import * as React from "react";
 import { ReactNode } from "react";
-import AceEditor from "react-ace";
+import { UnControlled as CodeMirror } from "react-codemirror2";
 import styled from "styled-components";
 import { StyleConstants } from "../../../styling/StyleConstants";
 
-const ace = brace;
-
 const CodeEditorContainer = styled.div`
-  #engraved-code-field {
+  position: relative;
+
+  .CodeMirror {
     border: 1px solid ${StyleConstants.colors.discreet};
+    font-size: 16px;
+    width: calc(100% - 2px);
   }
 `;
 
@@ -49,22 +42,13 @@ export class CodeEditor extends React.Component<ICodeEditorProps> {
   public render(): ReactNode {
     return (
       <CodeEditorContainer>
-        <AceEditor
-          mode={this.props.language}
-          theme="github"
-          width={"calc(100% - 2px)"}
-          minLines={5}
-          maxLines={50}
-          fontSize={14}
-          readOnly={this.props.isReadOnly}
-          showGutter={true}
-          showPrintMargin={false}
-          highlightActiveLine={true}
-          onChange={value => this.props.onValueChange(value)}
-          name="engraved-code-field"
-          defaultValue={this.props.value}
-          editorProps={{ $blockScrolling: true }}
-          setOptions={{ showLineNumbers: true }}
+        <CodeMirror
+          value={this.props.value}
+          onChange={(editor, data, value) => this.props.onValueChange(value)}
+          options={{
+            lineNumbers: true,
+            readOnly: this.props.isReadOnly
+          }}
         />
       </CodeEditorContainer>
     );
