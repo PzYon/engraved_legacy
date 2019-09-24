c12030501b53ce9eabd5655ae64714d1675c46fa
Markus Doggweiler <markus.doggweiler@gmail.com>
2019-09-24 22:15:27 +0200

    Add React.StrictMode to root component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,11 +5,13 @@ import { AuthenticationCallback } from "./authentication/AuthenticationCallback"
 
 export const App: React.FC = () => {
   return (
-    <Router>
-      <Switch>
-        <Route path="/authenticated/:jwt" component={AuthenticationCallback} exact={true} />
-        <Route path="/" component={AuthenticatedApp} />
-      </Switch>
-    </Router>
+    <React.StrictMode>
+      <Router>
+        <Switch>
+          <Route path="/authenticated/:jwt" component={AuthenticationCallback} exact={true} />
+          <Route path="/" component={AuthenticatedApp} />
+        </Switch>
+      </Router>
+    </React.StrictMode>
   );
 };
