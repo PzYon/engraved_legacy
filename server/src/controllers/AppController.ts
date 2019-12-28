0757a16e08cf14896396e4d77074824182cc1c92
Markus Doggweiler <markus.doggweiler@gmail.com>
2019-12-28 23:23:24 +0100

    Improving (and unifying) controller methods

--- server/src/controllers/AppController.ts
+++ server/src/controllers/AppController.ts
@@ -1,6 +1,6 @@
 import { IAppStats } from "engraved-shared";
 import { Express } from "express";
-import { Request, Response } from "express-serve-static-core";
+import { Request } from "express-serve-static-core";
 import { Db } from "mongodb";
 import { BaseAuthenticatedController } from "./BaseAuthenticatedController";
 
@@ -11,20 +11,18 @@ export class AppController extends BaseAuthenticatedController {
     this.authenticatedGet("/app/stats", this.getStats);
   }
 
-  private getStats = (req: Request, res: Response): void => {
-    Promise.all([
+  private getStats = (req: Request): Promise<IAppStats> => {
+    return Promise.all([
       this.createDbService(req).getTotalNumberOfUsers(),
       this.createDbService(req).getTotalNumberOfItems()
     ]).then(r => {
-      const o: IAppStats = {
+      return {
         commitHash: process.env.HEROKU_SLUG_COMMIT,
         appVersion: process.env.HEROKU_RELEASE_VERSION,
         releaseDate: process.env.HEROKU_RELEASE_CREATED_AT,
         numberOfRegisteredUsers: r[0],
         totalNumberOfItems: r[1]
       };
-
-      res.send(o);
     });
   };
 }
